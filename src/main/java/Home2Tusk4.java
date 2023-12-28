835f5d7041921e61c46528256c17a85d89e268d2
KyryloCherednyk <cherednik38@gmail.com>
2023-12-28 16:02:02 +0200

    1

--- src/main/java/Home2Tusk4.java
+++ src/main/java/Home2Tusk4.java
@@ -1,3 +1,10 @@
 public class Home2Tusk4 {
+    public static void main(String[] args) {
+        int a = -5;
+        int b = 7;
+        int c = 11;
+        int minModulus = Math.min(a, Math.min (b, c));
+        System.out.println(Math.abs(minModulus));
+    }
 
 }
