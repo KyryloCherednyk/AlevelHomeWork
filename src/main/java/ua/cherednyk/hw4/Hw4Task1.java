1c558f4b3e4176f62bbe696728e0f88a6239de5d
KyryloCherednyk <cherednik38@gmail.com>
2024-01-15 14:11:38 +0200

    1

--- src/main/java/ua/cherednyk/hw4/Hw4Task1.java
+++ src/main/java/ua/cherednyk/hw4/Hw4Task1.java
@@ -1,5 +1,7 @@
 package ua.cherednyk.hw4;
+
 import java.util.concurrent.ThreadLocalRandom;
+
 public class Hw4Task1 {
     public static void main(String[] args) {
         int[] array = new int[400];
@@ -11,18 +13,21 @@ public class Hw4Task1 {
         double geometricMean = calculateGeometricMean(array);
         System.out.println("Среднее геометрическое: " + geometricMean);
     }
+
     public static int[] generateRandomArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
             array[i] = ThreadLocalRandom.current().nextInt(1, 10);
         }
         return array;
     }
+
     public static void printArray(int[] array) {
         for (int element : array) {
             System.out.print(element + " ");
         }
         System.out.println();
     }
+
     public static double calculateArithmeticMean(int[] array) {
         int sum = 0;
         for (int element : array) {
--- src/main/java/ua/cherednyk/hw4/Hw4Task2.java
+++ src/main/java/ua/cherednyk/hw4/Hw4Task2.java
@@ -1,6 +1,8 @@
 package ua.cherednyk.hw4;
+
 import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
+
 public class Hw4Task2 {
     public static void main(String[] args) {
         int[] array = new int[400];
@@ -10,18 +12,21 @@ public class Hw4Task2 {
         int primeCount = countPrimeNumbers(array);
         System.out.println("Количество простых чисел в массиве: " + primeCount);
     }
+
     public static int[] generateRandomArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
             array[i] = ThreadLocalRandom.current().nextInt(1, 1000);
         }
         return array;
     }
+
     public static void printArray(int[] array) {
         for (int element : array) {
             System.out.print(element + " ");
         }
         System.out.println();
     }
+
     public static boolean isPrime(int number) {
         if (number <= 1) {
             return false;
@@ -33,6 +38,7 @@ public class Hw4Task2 {
         }
         return true;
     }
+
     public static int countPrimeNumbers(int[] array) {
         int count = 0;
         for (int num : array) {
