90d8d10f470a1261f7e0fd7dbd9359325baca447
KyryloCherednyk <cherednik38@gmail.com>
2024-01-26 09:18:25 +0200

    hw7tusk2 first commit

--- src/main/java/ua/cherednyk/hw7/Vector3d.java
+++ src/main/java/ua/cherednyk/hw7/Vector3d.java
@@ -2,10 +2,10 @@ package ua.cherednyk.hw7;
 
 import java.util.Random;
 
-public class Vector3D {
+public class Vector3d {
     public double x, y, z;
 
-    public Vector3D(double x, double y, double z) {
+    public Vector3d(double x, double y, double z) {
         this.x = x;
         this.y = y;
         this.z = z;
@@ -15,19 +15,19 @@ public class Vector3D {
         return Math.sqrt(x * x + y * y + z * z);
     }
 
-    public Vector3D crossProduct(Vector3D otherVector) {
-        return new Vector3D(
+    public Vector3d crossProduct(Vector3d otherVector) {
+        return new Vector3d(
                 y * otherVector.z - z * otherVector.y,
                 z * otherVector.x - x * otherVector.z,
                 x * otherVector.y - y * otherVector.x
         );
     }
 
-    public double dotProduct(Vector3D otherVector) {
+    public double dotProduct(Vector3d otherVector) {
         return x * otherVector.x + y * otherVector.y + z * otherVector.z;
     }
 
-    public double angleCosine(Vector3D otherVector) {
+    public double angleCosine(Vector3d otherVector) {
         double dotProduct = dotProduct(otherVector);
         double magnitudeProduct = length() * otherVector.length();
         if (magnitudeProduct == 0) {
@@ -36,19 +36,19 @@ public class Vector3D {
         return dotProduct / magnitudeProduct;
     }
 
-    public Vector3D vectorSum(Vector3D otherVector) {
-        return new Vector3D(x + otherVector.x, y + otherVector.y, z + otherVector.z);
+    public Vector3d vectorSum(Vector3d otherVector) {
+        return new Vector3d(x + otherVector.x, y + otherVector.y, z + otherVector.z);
     }
 
-    public Vector3D vectorDifference(Vector3D otherVector) {
-        return new Vector3D(x - otherVector.x, y - otherVector.y, z - otherVector.z);
+    public Vector3d vectorDifference(Vector3d otherVector) {
+        return new Vector3d(x - otherVector.x, y - otherVector.y, z - otherVector.z);
     }
 
-    public static Vector3D[] generateRandomVectors(int N) {
-        Vector3D[] randomVectors = new Vector3D[N];
+    public static Vector3d[] generateRandomVectors(int N) {
+        Vector3d[] randomVectors = new Vector3d[N];
         Random random = new Random();
         for (int i = 0; i < N; i++) {
-            randomVectors[i] = new Vector3D(
+            randomVectors[i] = new Vector3d(
                     random.nextDouble() * 20 - 10,
                     random.nextDouble() * 20 - 10,
                     random.nextDouble() * 20 - 10
@@ -62,8 +62,8 @@ public class Vector3D {
     }
 
     public static void main(String[] args) {
-        Vector3D vector1 = new Vector3D(1, 2, 3);
-        Vector3D vector2 = new Vector3D(4, 5, 6);
+        Vector3d vector1 = new Vector3d(1, 2, 3);
+        Vector3d vector2 = new Vector3d(4, 5, 6);
 
         System.out.println("Vector 1 length: " + vector1.length());
         System.out.println("Vector 2 length: " + vector2.length());
@@ -76,7 +76,7 @@ public class Vector3D {
 
         System.out.println("Angle cosine: " + vector1.angleCosine(vector2));
 
-        Vector3D[] randomVectors = Vector3D.generateRandomVectors(3);
+        Vector3d[] randomVectors = Vector3d.generateRandomVectors(3);
         for (int i = 0; i < randomVectors.length; i++) {
             System.out.println("Random Vector " + (i + 1) + ": " + randomVectors[i]);
         }
