19d46b4dfe350d392b6ea4c5f06b6cbc9ef038ff
KyryloCherednyk <cherednik38@gmail.com>
2024-01-30 20:48:36 +0200

    hw8 edit

--- src/main/java/ua/cherednyk/hw8/Student.java
+++ src/main/java/ua/cherednyk/hw8/Student.java
@@ -1,6 +1,32 @@
 package ua.cherednyk.hw8;
 
 public class Student {
+    String firstName;
+    String lastName;
+    String group;
+    double averageMark;
 
+    public Student(String firstName, String lastName, String group, double averageMark) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.group = group;
+        this.averageMark = averageMark;
+    }
+
+    public double getScholarship() {
+        return (averageMark == 5) ? 100 : 80;
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student("John", "Doe", "GroupA", 4.5);
+        Aspirant aspirant = new Aspirant("Alice", "Smith", "GroupB", 5.0, "Research on Java");
+
+        Student[] students = {student, aspirant};
+
+        for (Student s : students) {
+            System.out.println(s.getScholarship());
+        }
+    }
+}
 
 
