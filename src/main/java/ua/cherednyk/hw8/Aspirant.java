14364769f5b7c5e51db25cbcc46f8f699617754c
KyryloCherednyk <cherednik38@gmail.com>
2024-01-30 20:48:09 +0200

    hw8 edit

--- src/main/java/ua/cherednyk/hw8/Aspirant.java
+++ src/main/java/ua/cherednyk/hw8/Aspirant.java
@@ -1,6 +1,18 @@
 package ua.cherednyk.hw8;
 
 public class Aspirant extends Student {
+    String scientificWork;
 
+    public Aspirant(String firstName, String lastName, String group, double averageMark, String scientificWork) {
+        super(firstName, lastName, group, averageMark);
+        this.scientificWork = scientificWork;
+    }
+
+    @Override
+    public double getScholarship() {
+        return (averageMark == 5) ? 200 : 180;
+    }
+
+}
 
 
