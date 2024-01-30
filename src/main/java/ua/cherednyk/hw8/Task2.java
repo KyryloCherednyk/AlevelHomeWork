51107958d904030c6ab7e7bab1a91edb5dd50b04
KyryloCherednyk <cherednik38@gmail.com>
2024-01-30 20:49:20 +0200

    hw8 edit

--- src/main/java/ua/cherednyk/hw8/Task2.java
+++ src/main/java/ua/cherednyk/hw8/Task2.java
@@ -2,5 +2,28 @@ package ua.cherednyk.hw8;
 
 
 @FunctionalInterface
-interface Pow
+interface Pow {
+    int pow(int number, int exponent);
+}
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        Pow powFunction = (number, exponent) -> {
+            int result = 1;
+
+            for (int i = 0; i < exponent; i++) {
+                result *= number;
+            }
+
+            return result;
+        };
+
+        int base = 2;
+        int exponent = 3;
+        int result = powFunction.pow(base, exponent);
+
+        System.out.println(base + " в степени " + exponent + " равно " + result);
+    }
+}
 
