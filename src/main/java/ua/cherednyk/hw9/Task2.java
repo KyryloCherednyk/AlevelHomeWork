d172c16c090f30583557efa8b578b032269250dc
KyryloCherednyk <cherednik38@gmail.com>
2024-02-01 10:00:23 +0200

    New Hw9

---
+++ src/main/java/ua/cherednyk/hw9/Task2.java
@@ -0,0 +1,38 @@
+package ua.cherednyk.hw9;
+
+public class Task2 {
+    public static void main(String[] args) {
+        try {
+            f();
+        } catch (CustomException2 ce2) {
+            System.out.println("Перехвачено исключение CustomException2 в методе main: " + ce2.getMessage());
+        }
+    }
+
+    public static void f() throws CustomException2 {
+        try {
+            g();
+        } catch (CustomException1 ce1) {
+            System.out.println("Перехвачено исключение CustomException1 в методе f: " + ce1.getMessage());
+            // Бросаем другое исключение
+            throw new CustomException2("Исключение CustomException2 из метода f", ce1);
+        }
+    }
+
+    public static void g() throws CustomException1 {
+        // Генерируем исключение CustomException1
+        throw new CustomException1("Исключение CustomException1 из метода g");
+    }
+}
+
+class CustomException1 extends Exception {
+    public CustomException1(String message) {
+        super(message);
+    }
+}
+
+class CustomException2 extends Exception {
+    public CustomException2(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
